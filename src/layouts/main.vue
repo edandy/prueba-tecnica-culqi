b8b8ece26d5a816c0d92f6e67dff449123a11520
Dandy <edandy.perez@gmail.com>
2023-11-21 20:54:52 -0500

    afinando detalles de maquetacion

--- src/layouts/main.vue
+++ src/layouts/main.vue
@@ -4,17 +4,25 @@
     import { useAuthStore } from '@/stores/auth'
     import router from "@/router";
     import {storeToRefs} from "pinia";
+    import {ref} from "vue";
+    import type {Ref} from "vue";
     const { REMOVE_TOKEN } = useAuthStore()
     const { user, getInitials } = storeToRefs(useAuthStore())
+
+    const sidebar: Ref<boolean> = ref(false)
 
     const logout = async () => {
         await REMOVE_TOKEN()
         await router.push('/')
     }
+
+    const showSidebar = () => {
+        sidebar.value = !sidebar.value
+    }
 </script>
 
 <template>
-    <aside class="ml-[-100%] fixed z-20 top-0 pb-3 px-6 w-full flex flex-col justify-between h-screen shadow transition duration-300 md:w-4/12 lg:ml-0 lg:w-[25%] xl:w-[20%] 2xl:w-[15%] bg-white">
+    <aside class="ml-[-100%] fixed z-20 top-0 pb-3 px-6 w-full flex flex-col justify-between h-screen shadow transition duration-300 md:w-4/12 lg:ml-0 lg:w-[25%] xl:w-[20%] 2xl:w-[15%] bg-white" :class="sidebar ? 'ml-0':''">
         <div>
             <div class="-mx-6 px-6 py-4">
                 <a href="#" title="home">
@@ -46,8 +54,8 @@
     </aside>
     <div class="ml-auto lg:w-[75%] xl:w-[80%] 2xl:w-[85%] bg-gray-50">
         <div class="sticky z-10 top-0 h-16 shadow bg-white lg:py-2.5 flex items-center">
-            <div class="px-6 flex items-center justify-between w-full">&nbsp;
-                <button class="w-12 h-16 -mr-2 border-r lg:hidden">
+            <div class="px-6 flex items-center justify-between w-full">
+                <button class="w-12 h-16 -mr-2 border-r lg:hidden" @click.prevent="showSidebar">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 my-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                     </svg>
--- src/views/EmployeesView.vue
+++ src/views/EmployeesView.vue
@@ -86,8 +86,8 @@
         <skeleton v-if="loading" title="Empleados" />
 
         <div v-else class="bg-white p-4 rounded-lg">
-            <div class="flex items-center justify-between mb-4 text-sm">
-                <div class="">
+            <div class="md:flex items-center justify-between mb-4 text-sm">
+                <div class="mb-2 md:mb-0">
                     <h2 class="text-lg font-semibold mb-0">Empleados</h2>
                     <p class="text-gray-600">Gestionar empleados</p>
                 </div>
@@ -107,20 +107,20 @@
                 </div>
             </div>
 
-            <div class="flex mb-3 text-sm items-center justify-between gap-4">
-                <div class="relative w-9/12 group">
+            <div class="md:flex mb-3 text-sm items-center justify-between gap-4">
+                <div class="relative w-full lg:w-9/12 group mb-2 md:mb-0">
                     <input type="search" id="search" class="w-full pl-3 pr-10 py-3 border border-gray-200 rounded-lg hover:border-secondary focus:outline-none focus:border-secondary transition-colors" placeholder="Buscar empleado">
                     <button class="block w-7 h-7 text-center text-xl leading-0 absolute top-2 right-2 text-gray-400 focus:outline-none hover:text-gray-900 transition-colors">
                         <SearchIcon fillClass="fill-primary group-hover:fill-secondary"/>
                     </button>
                 </div>
 
-                <div class="dropdown inline-block relative w-3/12">
+                <div class="dropdown inline-block relative w-full lg:w-3/12">
                     <button class="border text-gray-700 py-3 px-4 rounded-lg inline-flex items-center w-full justify-between">
                         <span class="mr-1">Nombre de cargo</span>
                         <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/> </svg>
                     </button>
-                    <ul class="dropdown-menu bg-white shadow absolute hidden text-gray-700 w-full rounded">
+                    <ul class="dropdown-menu z-10 bg-white shadow absolute hidden text-gray-700 w-full rounded">
                         <li class=""><a class="rounded-t hover:bg-gray-100 py-3 px-4 block whitespace-no-wrap" href="#">UI UX Designer</a></li>
                         <li class=""><a class="hover:bg-gray-100 py-3 px-4 block whitespace-no-wrap" href="#">Graphic Designer</a></li>
                         <li class=""><a class="hover:bg-gray-100 py-3 px-4 block whitespace-no-wrap" href="#">Finance</a></li>
@@ -130,60 +130,62 @@
                 </div>
             </div>
 
-            <table class="w-full table-auto text-sm text-gray-600 text-left mb-5">
-                <thead class="bg-gray-500x rounded-lg">
-                    <tr class="text-sm leading-normal">
-                        <th class="py-4 px-4 rounded-l-xl bg-gray-50 font-medium text-sm text-left">Nombre</th>
-                        <th class="py-4 px-4 bg-gray-50 font-medium text-sm">Nombre de cargo</th>
-                        <th class="py-4 px-4 bg-gray-50 font-medium text-sm">Departamento</th>
-                        <th class="py-4 px-4 bg-gray-50 font-medium text-sm">Oficina</th>
-                        <th class="py-4 px-4 bg-gray-50 font-medium text-sm">Cuenta</th>
-                        <th class="py-4 px-4 rounded-r-xl bg-gray-50 font-medium text-sm text-right">Acciones</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr class="hover:bg-grey-lighter" v-for="employee in employees" :key="employee.id">
-                        <td class="py-3 px-4 border-b border-gray-100">
-                            <p>
-                                {{ employee.nombre }}
-                            </p>
+            <div class="relative overflow-x-auto mb-3">
+                <table class="w-full table-auto text-sm text-gray-600 text-left mb-5">
+                    <thead class="bg-gray-500x rounded-lg">
+                        <tr class="text-sm leading-normal">
+                            <th class="py-4 px-4 rounded-l-xl bg-gray-50 font-medium text-sm text-left">Nombre</th>
+                            <th class="py-4 px-4 bg-gray-50 font-medium text-sm">Nombre de cargo</th>
+                            <th class="py-4 px-4 bg-gray-50 font-medium text-sm">Departamento</th>
+                            <th class="py-4 px-4 bg-gray-50 font-medium text-sm">Oficina</th>
+                            <th class="py-4 px-4 bg-gray-50 font-medium text-sm">Cuenta</th>
+                            <th class="py-4 px-4 rounded-r-xl bg-gray-50 font-medium text-sm text-right">Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="hover:bg-grey-lighter" v-for="employee in employees" :key="employee.id">
+                            <td class="py-3 px-4 border-b border-gray-100">
+                                <p>
+                                    {{ employee.nombre }}
+                                </p>
 
-                            <a :href="`mailto:${employee.correo}`" class="text-xs text-gray-400" target="_blank">
-                                {{ employee.correo }}
-                            </a>
-                        </td>
-                        <td class="py-3 px-4 border-b border-gray-100">{{ employee.cargo }}</td>
-                        <td class="py-3 px-4 border-b border-gray-100">{{ employee.departamento }}</td>
-                        <td class="py-3 px-4 border-b border-gray-100">{{ employee.oficina }}</td>
-                        <td class="py-3 px-4 border-b border-gray-100">{{ employee.estadoCuenta }}</td>
-                        <td class="py-3 px-4 border-b border-gray-100 text-right">
-                            <div class="flex justify-end gap-2">
-                                <button
-                                    type="button"
-                                    class="tracking-wide bg-green-500 text-gray-100 px-2 py-1 rounded-md hover:bg-black transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none"
-                                >
-                                    <ViewIcon />
-                                </button>
-                                <button
-                                    type="button"
-                                    class="tracking-wide bg-blue-500 text-gray-100 px-2 py-1 rounded-md hover:bg-black transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none"
-                                >
-                                    <EditIcon />
-                                </button>
-                                <button
-                                    type="button"
-                                    class="tracking-wide bg-red-500 text-gray-100 px-2 py-1 rounded-md hover:bg-black transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none"
-                                >
-                                    <DeleteIcon />
-                                </button>
-                            </div>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+                                <a :href="`mailto:${employee.correo}`" class="text-xs text-gray-400" target="_blank">
+                                    {{ employee.correo }}
+                                </a>
+                            </td>
+                            <td class="py-3 px-4 border-b border-gray-100">{{ employee.cargo }}</td>
+                            <td class="py-3 px-4 border-b border-gray-100">{{ employee.departamento }}</td>
+                            <td class="py-3 px-4 border-b border-gray-100">{{ employee.oficina }}</td>
+                            <td class="py-3 px-4 border-b border-gray-100">{{ employee.estadoCuenta }}</td>
+                            <td class="py-3 px-4 border-b border-gray-100 text-right">
+                                <div class="flex justify-end gap-2">
+                                    <button
+                                        type="button"
+                                        class="tracking-wide bg-green-500 text-gray-100 px-2 py-1 rounded-md hover:bg-black transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none"
+                                    >
+                                        <ViewIcon />
+                                    </button>
+                                    <button
+                                        type="button"
+                                        class="tracking-wide bg-blue-500 text-gray-100 px-2 py-1 rounded-md hover:bg-black transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none"
+                                    >
+                                        <EditIcon />
+                                    </button>
+                                    <button
+                                        type="button"
+                                        class="tracking-wide bg-red-500 text-gray-100 px-2 py-1 rounded-md hover:bg-black transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none"
+                                    >
+                                        <DeleteIcon />
+                                    </button>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
 
-            <div class="flex justify-between items-center text-sm">
-                <nav v-if="filters.limit < total" aria-label="Pagination" class="flex items-center text-gray-600 gap-2">
+            <div class="md:flex justify-between items-center text-sm">
+                <nav v-if="filters.limit < total" aria-label="Pagination" class="flex justify-center items-center text-gray-600 gap-2 mb-6 md:mb-0">
                     <a v-if="filters.page > 1" @click.prevent="setPage(1)" href="#" class="p-1 mr-3 rounded border border-gray-200 hover:bg-gray-100">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
@@ -206,7 +208,7 @@
                     </a>
                 </nav>
                 <div class="text-gray-600 flex justify-between gap-3 items-center">
-                    <p>Mostrando {{ to }} a {{ from }} de {{ total }} registros</p>
+                    <p class="w-1/2 md:w-auto">Mostrando {{ to }} a {{ from }} de {{ total }} registros</p>
                     <div class="dropdown inline-block relative">
                         <ul class="dropdown-menu bg-white shadow absolute bottom-7 hidden text-gray-700 w-full rounded-lg">
                             <li class=""><a class="rounded-t-lg hover:bg-gray-100 py-2 px-4 block whitespace-no-wrap" href="#" @click.prevent="setLimit(10)">10</a></li>
